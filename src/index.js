c544c5d0068838d2e5a48b0cb6a8b3f8f9757b5b
mustafababeli <252592632+mustafababily@users.noreply.github.com>
2026-02-21 04:12:30 +0300

    added dialog to (add project btn)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 import "./styles.css";
-import Project from "./project.js";
 import Todo from "./todo.js";
+import Project from "./project.js";
+
+const addProjectBtn = document.getElementById("my-projects");
+const dialog = document.getElementById("add-project");
+const form = document.getElementById("project-form");
+const input = document.getElementById("project-input");
+
+const projects = [];
+
+addProjectBtn.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const projectName = input.value.trim();
+  if (!projectName) return;
+
+  const newProject = new Project(projectName);
+  projects.push(newProject);
+
+  console.log(projects);
+
+  input.value = "";
+  dialog.close();
+});
