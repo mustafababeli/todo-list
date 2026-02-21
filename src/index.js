e9dc7db981ea9872e8c0b8e52dc736e92c9b3f6a
mustafababeli <252592632+mustafababily@users.noreply.github.com>
2026-02-22 00:01:29 +0300

    Added todo form and wired it to JS

--- src/index.js
+++ src/index.js
@@ -2,18 +2,28 @@ import "./styles.css";
 import Todo from "./todo.js";
 import Project from "./project.js";
 
-const addProjectBtn = document.getElementById("my-projects");
-const dialog = document.getElementById("add-project");
-const form = document.getElementById("project-form");
-const input = document.getElementById("project-input");
+/* =========================
+   PROJECT STUFF
+========================= */
+
+const addProjectBtn = document.getElementById("btn-add-project");
+const projectDialog = document.getElementById("project-dialog");
+const projectForm = document.getElementById("project-form");
+const input = document.getElementById("project-name-input");
+const projectCancelBtn = document.querySelector(".btn-cancel-project");
+const projectDropdown = document.getElementById("projects-dropdown");
 
 const projects = [];
 
-addProjectBtn.addEventListener("click", () => {
-  dialog.showModal();
+projectCancelBtn.addEventListener("click", () => {
+  projectDialog.close();
 });
 
-form.addEventListener("submit", (e) => {
+addProjectBtn.addEventListener("click", () => {
+  projectDialog.showModal();
+});
+
+projectForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
   const projectName = input.value.trim();
@@ -22,8 +32,46 @@ form.addEventListener("submit", (e) => {
   const newProject = new Project(projectName);
   projects.push(newProject);
 
-  console.log(projects);
+  const option = document.createElement("option");
+  option.value = projectName;
+  option.textContent = projectName;
+  projectDropdown.appendChild(option);
 
   input.value = "";
-  dialog.close();
+  projectDialog.close();
+});
+
+/* =========================
+   TODO STUFF
+========================= */
+
+const addTodoBtn = document.getElementById("btn-add-todo");
+const todoDialog = document.getElementById("todo-dialog");
+const todoForm = document.getElementById("todo-form");
+const todoCancelBtn = document.getElementById("btn-cancel-todo");
+const todoName = document.getElementById("todo-name-input");
+const todoDescriptoin = document.getElementById("todo-description-input");
+const dueDate = document.getElementById("todo-dueDate-input");
+
+todoForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const priority = document.querySelector(
+    'input[name="todo-priority"]:checked',
+  )?.value;
+
+  const newTodo = new Todo(
+    todoName.value,
+    todoDescriptoin.value,
+    dueDate.value,
+    priority,
+  );
+  console.log(newTodo);
+});
+
+addTodoBtn.addEventListener("click", () => {
+  todoDialog.showModal();
+});
+
+todoCancelBtn.addEventListener("click", () => {
+  todoDialog.close();
 });
